cb2cc345a83cd80c5292263ac79e38c0d3eb136b
Mohamedi81 <Simplym@icloud.com>
2026-04-23 21:46:39 +0200

    Align package to com.mohamedi.cobolmigration as per architecture blueprint

---
+++ src/main/java/com/mohamedi/cobolmigration/CobolMigrationApplication.java
@@ -0,0 +1,12 @@
+package com.mohamedi.cobolmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobolMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobolMigrationApplication.class, args);
+    }
+}
